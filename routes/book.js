211e8ddfdcdd145b59d19f2c1a24f26d5e4741a3
bochipachibuchi <shin.supernoob@gmail.com>
2020-02-19 18:52:47 +0900

    Added getBookDetail API

--- routes/book.js
+++ routes/book.js
@@ -15,6 +15,8 @@ const api = axios.create({
   }
 });
 
+//getBook
+//OpenAPI 통한 책 검색 API : 클라이언트에서 키워드를 받아 큐레이션.
 router.get("/:title/:display", function(req, res) {
   const {
     params: { title: title, display: display }
@@ -37,6 +39,35 @@ router.get("/:title/:display", function(req, res) {
   showBook(title, display);
 });
 
+//getAllBook
+//전체 책 정보 제공 API : DB 에 저장된 책 정보들 제공.
+router.get("/", function(req, res) {
+  Book.find(function(err, books) {
+    if (books) {
+      res.status(200).json({ success: true, msg: "성공!!", books });
+    } else {
+      res.status(400).send({ success: false, msg: err });
+    }
+  });
+});
+
+//getBookDetail
+// 선택 된 책(Single) 정보 제공 API : isbn 키워드로 DB에서 해당되는 책 정보를 찾아 클라이언트에게 제공.
+router.get("/:id", function(req, res) {
+  const {
+    params: { id: id }
+  } = req;
+  Book.findOne({ isbn: id }, function(err, book) {
+    if (book) {
+      res.status(200).json({ book: book, success: true, msg: "Sucess" });
+    } else {
+      res.status(400).json({ sucess: false, msg: err });
+    }
+  });
+});
+
+//addBook
+//책 추가 API : Add Book 스크린에서 선택된 책들 DB에 저장.
 router.post("/", function(req, res) {
   const dt = new Date();
   const books = req.body.map(item => {
@@ -54,16 +85,6 @@ router.post("/", function(req, res) {
         console.log(doc);
       });
       res.status(200).send({ success: true, msg: "성공!!", result: result });
-    }
-  });
-});
-
-router.get("/", function(req, res) {
-  Book.find(function(err, books) {
-    if (books) {
-      res.status(200).json({ success: true, msg: "성공!!", books });
-    } else {
-      res.status(400).send({ success: false, msg: err });
     }
   });
 });
