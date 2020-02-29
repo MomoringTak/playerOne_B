b78596cb71569ca8204cb6888827a6cd23ac5ec7
bochipachibuchi <shin.supernoob@gmail.com>
2020-03-01 04:07:10 +0900

    [ADD] delete Comment API

--- routes/comment.js
+++ routes/comment.js
@@ -2,6 +2,7 @@ import express from "express";
 import Book from "../models/Book";
 import Comment from "../models/Comment";
 import db from "../db/db";
+import { restart } from "nodemon";
 
 const router = express.Router();
 
@@ -27,6 +28,23 @@ router.get("/:id", (req, res) => {
         res.status(400).json({ success: false, msg: err });
       }
     });
+});
+
+//deleteComment
+//해당 유저가 남긴 댓글을 삭제하는 API.
+router.delete("/", (req, res) => {
+  const {
+    query: { id }
+  } = req;
+  console.log(id);
+
+  Comment.deleteOne({ _id: id }, (err, deleteResult) => {
+    if (!err) {
+      res.status(200).json({ success: true, msg: "성공", deleteResult });
+    } else {
+      res.status(400).json({ success: false, msg: err });
+    }
+  });
 });
 
 export default router;
