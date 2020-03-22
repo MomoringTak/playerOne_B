d1ce48d124f38c9317d3952ebed95e7ac03e65c6
bochipachibuchi <shin.supernoob@gmail.com>
2020-03-22 15:17:16 +0900

    [MODIFIED] getBook from Naver with category

--- controllers/book/getBook.js
+++ controllers/book/getBook.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import cheerio from "cheerio";
+
+const UNDEFINED = "undefined";
 
 //네이버 검색 API
 const api = axios.create({
@@ -9,23 +12,63 @@ const api = axios.create({
   }
 });
 
+//네이버 책 API에 검색 키워드 콜
+const getNaverBook = (title, display) =>
+  api.get("", {
+    params: {
+      d_titl: title,
+      display: display
+    }
+  });
+
+//각 책들 링크에 기재되있는 제일 큰 카테고리 [예시) 소설->장르소설->추리 에서 소설
+const getCategory = async URL => {
+  try {
+    const html = await axios.get(URL);
+    const $ = await cheerio.load(html.data);
+
+    let data = {};
+
+    $("ul.history").each((i, elem) => {
+      data = {
+        container: $(elem)
+          .find("li")
+          .text()
+          .trim()
+      };
+    });
+
+    if (data.container === undefined) {
+      return UNDEFINED;
+    } else {
+      let category = data.container.replace(/\t|\n|>/g, "").split(" ");
+      return category[0];
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const addGenre = async books => {
+  for (let book of books) {
+    book.category = await getCategory(book.link);
+  }
+
+  return books;
+};
+
 const getBook = (req, res) => {
   const {
     params: { title, display }
   } = req;
 
-  const getBook = (title, display) =>
-    api.get("", {
-      params: {
-        d_titl: title,
-        display: display
-      }
-    });
-
   const showBook = async (title, display) => {
     try {
-      const { data } = await getBook(title, display);
-      res.status(200).json(data.items);
+      const { data } = await getNaverBook(title, display);
+
+      const bookResult = await addGenre(data.items);
+
+      res.status(200).json(bookResult);
     } catch (err) {
       res.status(400).json({ success: false, msg: err });
     }
