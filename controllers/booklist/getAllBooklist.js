7b9360cfcfe073c1af90bf95e1601e2dd1ff2903
bochipachibuchi <shin.supernoob@gmail.com>
2020-03-25 14:41:11 +0900

    [MODIFIED getAllBooklist w/ population

--- controllers/booklist/getAllBooklist.js
+++ controllers/booklist/getAllBooklist.js
@@ -1,10 +1,12 @@
 import BookList from "../../models/BookList";
 
 const getAllBooklist = async (req, res) => {
-  console.log("hello");
   try {
-    const BooklistResult = await BookList.find();
-    console.log(BooklistResult);
+    const BooklistResult = await BookList.find().populate({
+      path: "books",
+      model: "Book",
+      select: ["image", "title", "author", "publisher"]
+    });
     res.status(200).json({ success: true, msg: "성공", BooklistResult });
   } catch (err) {
     res.status(400).json({ success: false, msg: err });
