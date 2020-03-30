974bd057865471c8bf8622da1025cd4e86eebdc5
bochipachibuchi <shin.supernoob@gmail.com>
2020-03-31 05:48:09 +0900

    [DEPLOY] Fixed minor erros

--- src/db/db.js
+++ src/db/db.js
@@ -8,15 +8,15 @@ dotenv.config();
 // console.log(process.env.MONGO_URL);
 
 //localhost REst API server
-const url = "mongodb://localhost:27017/wtb";
+// const url = "mongodb://localhost:27017/wtb";
 
 //heroku Rest API server
 //QA끝나시면 아래 mongoose.connect(url, ) 이부분 커멘트 처리하시고 아래 production 커멘트 푸시면 됩니다... ㅎㅎㅎ
 //production development 동시에 처리하는 방법 알아보다가.. 실패....
 
 //production용 REstAPI
-// mongoose.connect(process.env.MONGO_URL, {
-mongoose.connect(url, {
+mongoose.connect(process.env.MONGO_URL, {
+  // mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
