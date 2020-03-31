506d1b87608c884ba9dbce5ffcf6732723f34e74
bochipachibuchi <shin.supernoob@gmail.com>
2020-04-01 03:56:31 +0900

    :(

--- src/db/db.js
+++ src/db/db.js
@@ -19,13 +19,13 @@ mongoose.connect(process.env.MONGO_URL, {
 });
 
 const db = mongoose.connection;
-db.once("open", _ => {
-  console.log("Database connected:", url);
-});
-
 // db.once("open", _ => {
-//   console.log("Database connected:", process.env.MONGO_URL);
+//   console.log("Database connected:", url);
 // });
+
+db.once("open", _ => {
+  console.log("Database connected:", process.env.MONGO_URL);
+});
 
 db.on("error", err => {
   console.error("connection error:", err);
